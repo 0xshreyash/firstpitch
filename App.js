452e5be753db27a1f691e990f214cb6e37dc6548
iot-anomaly <spatodia@student.unimelb.edu.au>
2018-12-20 09:35:11 +1100

    Restart button placement work and fix wave amplitude issues

--- App.js
+++ App.js
@@ -1,62 +1,89 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Dimensions,
+    Platform
 } from 'react-native';
 
 import RNSiriWaveView from 'react-native-siri-wave-view'
 
 export default class App extends Component<{}> {
-  constructor (props) {
-    super(props)
-    this.state = {
-      startAnimation: true,
-      stopAnimation: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            startAnimation: true,
+            stopAnimation: false,
+            screenWidth: Dimensions.get("window").width,
+            waveAmplitude: Platform.OS === 'ios' ? 1 : 100,
+            waveType: 0,
+            numberOfWaves: 1,
+            waveWidth: Platform.OS === 'ios' ? 3 : 250
+        }
     }
-  }
 
-  render() {
-    return <View style={styles.container}>
-      <RNSiriWaveView type={0} width={800} height={750} startAnimation={this.state.startAnimation} stopAnimation={this.state.stopAnimation} />
+    render() {
+        return <View style={styles.container}>
+            <View style={[styles.topMenuContainer]}>
 
-      <TouchableOpacity style={[styles.button]} onPress={() => {
-        this.setState({
-          startAnimation: true,
-          stopAnimation: false
-        });
-      }}>
-        <Text>Start</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={[styles.button]} onPress={() => {
-        this.setState({
-          startAnimation: false,
-          stopAnimation: true
-        });
-      }}>
-        <Text>Stop</Text>
-      </TouchableOpacity>
-    </View>;
-  }
+            </View>
+            <View style={[styles.waveContainer]}>
+                <RNSiriWaveView height={300} width={this.state.screenWidth} amplitude={this.state.waveAmplitude}
+                                type={this.state.waveType} numberOfWaves={this.state.numberOfWaves}
+                                primaryWaveLineWidth={this.state.waveWidth}
+                                startAnimation={this.state.startAnimation} stopAnimation={this.state.stopAnimation}/>
+            </View>
+            <View style={[styles.buttonContainer]}>
+                <TouchableOpacity style={[styles.button]} onPress={() => {
+                    this.setState({
+                        startAnimation: true,
+                        stopAnimation: false
+                    });
+                }}>
+                    <Text>Start</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button]} onPress={() => {
+                    this.setState({
+                        startAnimation: false,
+                        stopAnimation: true
+                    });
+                }}>
+                    <Text>Stop</Text>
+                </TouchableOpacity>
+            </View>
+        </View>;
+    }
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-    flexDirection: "column"
-  },
-  button: {
-    height: 40,
-    width: 100,
-    backgroundColor: "#add8e6",
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 10
-  }
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "stretch",
+        backgroundColor: "#FFFFFF",
+    },
+    topMenuContainer: {
+        flex: 1,
+        alignItems: "stretch"
+    },
+    waveContainer: {
+        flex: 3,
+        justifyContent: "center",
+        alignItems: "stretch"
+    },
+    buttonContainer: {
+        flex: 3,
+        alignItems: "stretch"
+    },
+    button: {
+        height: 40,
+        width: 100,
+        backgroundColor: "#add8e6",
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10
+    }
 });
--- android/app/src/main/java/com/firstpitch/MainApplication.java
+++ android/app/src/main/java/com/firstpitch/MainApplication.java
@@ -3,6 +3,7 @@ package com.firstpitch;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
 import ui.siriwave.RNSiriWaveViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSoundPlayerPackage(),
             new RNSiriWaveViewPackage()
       );
     }
